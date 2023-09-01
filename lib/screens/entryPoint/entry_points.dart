6f95a932e2eccfce9ec2d1d54a85dc7c2239f53f
Andrew <andysooho@gmail.com>
2023-09-02 00:49:50 +0900

    bottomNav 아이콘 작업

--- lib/screens/entryPoint/entry_points.dart
+++ lib/screens/entryPoint/entry_points.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'package:rive_animation/constants.dart';
+import 'package:rive_animation/utils/rive_utils.dart';
 
 class EntryPoint extends StatefulWidget {
   const EntryPoint({super.key});
@@ -14,6 +16,87 @@ class _EntryPointState extends State<EntryPoint> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: backgroundColor2.withOpacity(0.8),
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ...List.generate(
+                5,
+                (index) => GestureDetector(
+                  onTap: () {
+                    bottomNavs[index].input!.change(true);
+                    Future.delayed(const Duration(seconds: 1), () {
+                      bottomNavs[index].input!.change(false);
+                    });
+                  },
+                  child: SizedBox(
+                    height: 36,
+                    width: 36,
+                    child: RiveAnimation.asset(
+                      "assets/RiveAssets/icons.riv",
+                      artboard: bottomNavs[index].artboard,
+                      onInit: (artboard) {
+                        StateMachineController controller =
+                            RiveUtils.getRiveController(artboard,
+                                stateMachineName:
+                                    bottomNavs[index].stateMachineName);
+                        bottomNavs[index].input =
+                            controller.findSMI("active") as SMIBool;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class RiveAsset {
+  final String artboard, stateMachineName, title, src;
+  late SMIBool? input;
+
+  RiveAsset(
+    this.src, {
+    required this.artboard,
+    required this.stateMachineName,
+    required this.title,
+    this.input,
+  });
+
+  set setInput(SMIBool status) {
+    input = status;
+  }
+}
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "CHAT", stateMachineName: "CHAT_Interactivity", title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "SEARCH",
+      stateMachineName: "SEARCH_Interactivity",
+      title: "Search"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "TIMER",
+      stateMachineName: "TIMER_Interactivity",
+      title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "BELL",
+      stateMachineName: "BELL_Interactivity",
+      title: "Notifications"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "USER",
+      stateMachineName: "USER_Interactivity",
+      title: "Profile"),
+];
